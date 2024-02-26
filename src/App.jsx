86582aba81ad04ef4c93daf9d777d7132c237009
AzizJlassi <aziz.jlassi2233@gmail.com>
2024-02-26 10:23:01 +0100

    list with react

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,56 @@ import './App.css'
 
 
 import * as React from "react";
+const list = [
+  {
+    title:"react",
+    url:"https://reactjs.org/",
+    author:"Jordan Walke",
+    num_comments: 3,
+    points:4,
+    objectId:0,
+  },
+  {
+    title:"Redux",
+    url:"https://redux.js.org/",
+    author:"Dan Ambrov,Andrew Clark",
+    num_comments: 2,
+    points:5,
+    objectId:1,
+  },
+
+]
 function App() {
   
 
   return (
     <>
       <div>
-        <h1> hello world {title}</h1>
+        <h1> React App </h1>
+        <label htmlFor="search">Search</label>
+        <input id="search" type="text"/>
+        <hr/>
+        <ul>
+        {list.map(function (item){
+          return (
+          <li key={item.objectId}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          
+          
+          </li>
+          );
+        })}
+        </ul>
       </div>
+
        
     </>
-  )
+  );
 }
 
 export default App
