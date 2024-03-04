2f5baab74b0eddaa4c95a37c87bc9e09a35321d7
AzizJlassi <aziz.jlassi2233@gmail.com>
2024-03-04 10:22:42 +0100

    multi fun

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,82 @@ import './App.css'
 import * as React from "react";
 const list = [
   {
-    title:"react",
-    url:"https://reactjs.org/",
-    author:"Jordan Walke",
+    title: "react",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
-    points:4,
-    objectId:0,
+    points: 4,
+    objectId: 0,
   },
   {
-    title:"Redux",
-    url:"https://redux.js.org/",
-    author:"Dan Ambrov,Andrew Clark",
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Ambrov,Andrew Clark",
     num_comments: 2,
-    points:5,
-    objectId:1,
+    points: 5,
+    objectId: 1,
   },
 
 ]
 function App() {
-  
+
 
   return (
     <>
       <div>
         <h1> React App </h1>
-        <label htmlFor="search">Search</label>
-        <input id="search" type="text"/>
-        <hr/>
-        <ul>
-        {list.map(function (item){
-          return (
-          <li key={item.objectId}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          
-          
-          </li>
-          );
-        })}
-        </ul>
+        <Search />
+        <hr />
+        <List />
       </div>
 
-       
     </>
   );
 }
+function Search() {
+  const handleChange = (event) => {
+    console.log(event)
+  }
+  return (
+    <div>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" onChange={handleChange} />
+
+    </div>
+
+
+
+  );
+
+}
+
+
+function List() {
+  return (
+    <div>
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectId}>
+              <span>
+                <a gref={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+    </div>
+
+
+
+  );
+
+}
+
+
 
 export default App
